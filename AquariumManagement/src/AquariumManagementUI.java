e81c7df3d5a9dbdffc056d5d9ffb08e9360b55f7
Christopher-Eunho <christopher.eunho@gmail.com>
2023-11-24 22:10:33 -0800

    Change the layout of home panel to accommodate more buttons in the future

--- AquariumManagement/src/AquariumManagementUI.java
+++ AquariumManagement/src/AquariumManagementUI.java
@@ -3,7 +3,6 @@ package AquariumManagement.src;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,7 +15,6 @@ public class AquariumManagementUI extends JFrame {
 
     private CardLayout cardLayout;
     private JPanel cardsPanel;
-    private Map<String, JPanel> categoryPanels; // HashMap to store category panels
     // declaring the database
     private AquariumManagementDB db;
 
@@ -24,7 +22,6 @@ public class AquariumManagementUI extends JFrame {
     public AquariumManagementUI() {
         super("Aquarium Manager");
         tablePackages = new ArrayList<TablePackage>();;
-        categoryPanels = new HashMap<>();
         connectDBPanel(); // connect DB and launch app
     }
 
